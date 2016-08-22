760b66300a4986aea64fc28162e6fdaea435755a
James Geddes <jamesg@muneris.io>
2016-08-22 15:37:39 +0800

    Refactor: Split into multiple Controllers

--- app/controllers/EntryController.scala
+++ app/controllers/EntryController.scala
@@ -2,18 +2,18 @@ package controllers
 
 import javax.inject.Inject
 
-import play.api._
-import play.api.mvc._
-import play.api.Play.current
-import play.api.i18n.Messages.Implicits._
-import play.twirl.api.Html
-import scala.concurrent.ExecutionContext.Implicits.global
+import dao.{CategoryDAO, EntryDAO}
+import models.{SearchForm, EntryFormData, EntryMaker, EntryForm}
 import play.api.libs.json.Json
+import play.api.mvc.{Action, Controller}
+import play.twirl.api.Html
+import play.api.Play.current
 
-import dao._
-import models._
+import play.api.i18n.Messages.Implicits._
+import scala.concurrent.ExecutionContext.Implicits.global
 
-class Application @Inject() (entryDAO: EntryDAO, categoryDAO: CategoryDAO) extends Controller {
+
+class EntryController @Inject() (entryDAO: EntryDAO, categoryDAO: CategoryDAO) extends Controller {
 
   def showEntries = Action.async { implicit request =>
     entryDAO.listWithCat map {entries =>
@@ -37,7 +37,8 @@ class Application @Inject() (entryDAO: EntryDAO, categoryDAO: CategoryDAO) exten
           println("description: " + formData.description)
           println("transaction: " + formData.transaction)
           entryDAO.add(EntryMaker(formData))
-          Redirect(routes.Application.showEntries())
+          //todo
+          Redirect(routes.EntryController.showEntries())
         }
       )
     }
@@ -59,7 +60,8 @@ class Application @Inject() (entryDAO: EntryDAO, categoryDAO: CategoryDAO) exten
           val form = EntryForm.form.fill(EntryFormData(entry.amount.abs, entry.description, if (entry.amount > 0) "1" else "-1", entry.catId))
           Ok(views.html.newEntry(form, categories, entryId))
         case None =>
-          Redirect(routes.Application.showEntries())
+          //todo
+          Redirect(routes.EntryController.showEntries())
       }
     }
   }
@@ -74,7 +76,8 @@ class Application @Inject() (entryDAO: EntryDAO, categoryDAO: CategoryDAO) exten
           println("description: " + formData.description)
           println("transaction: " + formData.transaction)
           entryDAO.update(entryId, EntryMaker(formData))
-          Redirect(routes.Application.showEntries())
+          //todo
+          Redirect(routes.EntryController.showEntries())
         }
       )
     }
@@ -83,7 +86,8 @@ class Application @Inject() (entryDAO: EntryDAO, categoryDAO: CategoryDAO) exten
   def deleteEntry(entryId: Long) = Action {
     println("Deleting a thing")
     entryDAO.delete(entryId)
-    Redirect(routes.Application.showEntries())
+    //todo
+    Redirect(routes.EntryController.showEntries())
   }
 
   def clearSearch = Action.async {
@@ -117,74 +121,4 @@ class Application @Inject() (entryDAO: EntryDAO, categoryDAO: CategoryDAO) exten
       }
     )
   }
-
-  def defineNewCategory = Action.async {
-    categoryDAO.listAll map {categories =>
-      Ok(views.html.newCategory(CategoryForm.form, categories))
-    }
-  }
-
-  def addCategory() = Action.async { implicit request =>
-      categoryDAO.listAll map { categories =>
-        CategoryForm.form.bindFromRequest.fold(
-          errorForm => BadRequest(views.html.newCategory(errorForm, categories)), // Handle error in form submission
-          formData => {
-            categories.exists(c => c.name.equals(formData.name)) match {
-              case true =>
-                println("Already have category named" + formData.name)
-                BadRequest(views.html.newCategory(CategoryForm.form.fill(formData), categories, msg = "Already have a Category named " + formData.name ))
-              case false =>
-                println ("new category form success!")
-                println ("name: " + formData.name)
-                println ("description: " + formData.description)
-                categoryDAO.add (Category (0, formData.name, formData.description) )
-                Redirect (routes.Application.showCategories() )
-            }
-          }
-        )
-      }
-  }
-
-  def editCategory(catId: Long) = Action.async {
-    for {
-      categories <- categoryDAO.listAll
-    } yield {
-      categories.find(c=> c.id == catId && c.id != 0) match {
-        case Some(cat) =>
-          val form = CategoryForm.form.fill(CategoryFormData(cat.name, cat.description))
-          Ok(views.html.newCategory(form, categories, catId))
-        case None =>
-          Redirect(routes.Application.showCategories())
-      }
-    }
-  }
-
-  def updateCategory(catId: Long) = Action.async { implicit request =>
-    categoryDAO.listAll map { categories =>
-      CategoryForm.form.bindFromRequest.fold(
-        errorForm => BadRequest(views.html.newCategory(errorForm, categories, catId)), // Handle error in form submission
-        formData => {
-          categories.exists(c => c.name.equals(formData.name) && c.id != catId) match {
-            case true =>
-              BadRequest(views.html.newCategory(CategoryForm.form.fill(formData), categories, catId, msg = "Already have a Category named " + formData.name ))
-            case false =>
-              categoryDAO.update(catId, formData)
-              Redirect(routes.Application.showCategories())
-          }
-        }
-      )
-    }
-  }
-
-  def deleteCategory(catId: Long) = Action {
-    categoryDAO.delete(catId)
-    Redirect(routes.Application.showCategories())
-  }
-
-  def showCategories = Action.async {
-    categoryDAO.listAll map { categories =>
-      Ok(views.html.categories(categories.filter(c=>c.id!=0)))
-    }
-  }
-
 }
